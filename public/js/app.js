bfd83a69a6a009815cd5e949da49297e8af27987
Ferew Tucho <ferewtucho@gmail.com>
2019-04-30 20:39:03 -0400

    changed colors

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 console.log("I'm Working");
 
-const form =  document.querySelector("button");
+const form = document.querySelector("button");
 const search = document.querySelector("input");
 
 const city = document.querySelector("#city-name");
@@ -13,35 +13,34 @@ const temp_min = document.querySelector("#low");
 
 const style = document.querySelector("#weather");
 
-
-
 form.addEventListener("click", e => {
-    e.preventDefault();
+  e.preventDefault();
 
-    console.log("I'm Clicked");
-  
-    const location = search.value;
-    //   console.log(location);
-  
-    // messageOne.textContent = "Loading...";
-    // messagetwo.textContent = "";
-  
-    fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
-      response.json().then(data => {
-        if (data.error) {
-          console.log(data.error);
+  console.log("I'm Clicked");
 
-        } else {
+  const location = search.value;
+  //   console.log(location);
 
-          city.textContent = data.name + ", " + data.sys.country;
-          icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-          style.style.backgroundImage = `url(./img/${data.weather[0].icon}.png)`;
+  // messageOne.textContent = "Loading...";
+  // messagetwo.textContent = "";
 
-          temperature.textContent = Math.round(data.main.temp);
-          temp_min.textContent =  Math.round(data.main.temp_min);
-          temo_max.textContent =  Math.round(data.main.temp_max);
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        city.textContent = data.name + ", " + data.sys.country;
+        icon.src = `http://openweathermap.org/img/w/${
+          data.weather[0].icon
+        }.png`;
+        style.style.backgroundImage = `linear-gradient(to right bottom, #416b779a, #80bbc48a), url(./img/${
+          data.weather[0].icon
+        }.png)`;
 
-        }
-      });
+        temperature.textContent = Math.round(data.main.temp);
+        temp_min.textContent = Math.round(data.main.temp_min);
+        temo_max.textContent = Math.round(data.main.temp_max);
+      }
     });
   });
+});
