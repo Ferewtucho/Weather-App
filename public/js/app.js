c788873a82b33636865757884df6da1830b8eb7d
Ferew Tucho <ferewtucho@gmail.com>
2019-05-03 09:45:09 -0400

    getting ready fo heroku delpoyment

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ form.addEventListener("click", e => {
 
   city.textContent = "Loading...";
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+  fetch(`/weather?address=${location}`).then(response => {
     response.json().then(data => {
       if (data.error) {
         console.log(data.error);
--- src/app.js
+++ src/app.js
@@ -57,5 +57,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Server is up on port 3000.");
+  console.log(`Server is up on port ${port}.`);
 });
